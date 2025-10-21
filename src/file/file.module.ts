25d0ca37f4e4a2846a30fef5b294978cf322fb31
馒头 <798521692@qq.com>
2025-10-21 16:22:18 +0800

    任务描述中添加图片删除图片在oss中体现出来

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OssFile } from './oss-file.entity';
 import { UserAvatar } from './user-avatar.entity';
 import { FileService } from './file.service';
+import { FileController } from './file.controller';
 import { OssConfig } from '../config/oss.config';
 
 @Module({
   imports: [TypeOrmModule.forFeature([OssFile, UserAvatar])],
+  controllers: [FileController],
   providers: [FileService, OssConfig],
   exports: [FileService],
 })
